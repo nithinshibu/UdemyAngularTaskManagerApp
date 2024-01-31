1a2ba7587ddb2325e2288663ff0bb9fd4b437d08
Nithin Mathew <nithin.mathew@sysfore.com>
2024-01-31 11:21:29 +0530

    Added some more content in the alert directive

--- src/app/alert.directive.ts
+++ src/app/alert.directive.ts
@@ -5,6 +5,7 @@ import {
   OnInit,
   HostListener,
   HostBinding,
+  Renderer2,
 } from '@angular/core';
 
 @Directive({
@@ -13,24 +14,44 @@ import {
 export class AlertDirective implements OnInit {
   @Input('error') error!: string;
   @HostBinding('title') title!: string;
-  constructor(private elementRef: ElementRef) {}
+  constructor(private elementRef: ElementRef, private renderer2: Renderer2) {}
+
+  divElement: any;
+  spanElement: any;
+  spanText: any;
+
   ngOnInit(): void {
-    this.elementRef.nativeElement.innerHTML = `
-    <div class="alert alert-danger fade show" role="alert" style="transition:transform 0.5s">
-    <span>${this.error}</span>
-    </div>
-    `;
+    this.divElement = this.renderer2.createElement('div');
+    this.renderer2.setAttribute(this.divElement, 'role', 'alert');
+    this.renderer2.setAttribute(
+      this.divElement,
+      'class',
+      'alert alert-danger fade show'
+    );
+    this.renderer2.setStyle(this.divElement, 'transition', 'transform 0.5s');
+    this.spanElement = this.renderer2.createElement('span');
+    this.renderer2.setAttribute(this.spanElement, 'class', 'message');
+
+    this.spanText = this.renderer2.createText(this.error);
+    this.renderer2.appendChild(this.spanElement, this.spanText);
+
+    this.renderer2.appendChild(this.divElement, this.spanElement);
+
+    this.elementRef.nativeElement.appendChild(this.divElement);
+    // this.elementRef.nativeElement.innerHTML = `
+    // <div class="alert alert-danger fade show" role="alert" style="transition:transform 0.5s">
+    // <span>${this.error}</span>
+    // </div>
+    // `;
     this.title = 'Please try again';
   }
 
   @HostListener('mouseenter', ['$event'])
   onMouseEnter(event: any) {
-    this.elementRef.nativeElement.querySelector('.alert').style.transform =
-      'scale(1.1)';
+    this.renderer2.setStyle(this.divElement, 'transform', 'scale(1.1)');
   }
   @HostListener('mouseleave', ['$event'])
   onMouseLeave(event: any) {
-    this.elementRef.nativeElement.querySelector('.alert').style.transform =
-      'scale(1)';
+    this.renderer2.setStyle(this.divElement, 'transform', 'scale(1)');
   }
 }
