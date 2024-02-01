677887d991e74d25346aa217d61c467bb6a7d229
Nithin Mathew <nithin.mathew@sysfore.com>
2024-02-01 15:16:01 +0530

    Added some content in master

--- src/app/admin/components/masters/masters.component.ts
+++ src/app/admin/components/masters/masters.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-masters',
   templateUrl: './masters.component.html',
-  styleUrl: './masters.component.scss'
+  styleUrl: './masters.component.scss',
 })
-export class MastersComponent {
-
+export class MastersComponent implements OnInit {
+  masterMenuItems = [
+    { itemName: 'Countries', displayName: 'Countries' },
+    { itemName: 'ClientLocations', displayName: 'Client Locations' },
+    { itemName: 'TaskPriorities', displayName: 'Task Priorities' },
+    { itemName: 'TaskStatus', displayName: 'Task Status' },
+  ];
+  activeItem!: string;
+  constructor() {}
+  ngOnInit(): void {}
 }
